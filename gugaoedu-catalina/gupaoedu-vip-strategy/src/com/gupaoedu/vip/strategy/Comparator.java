89e68df9862e52b6667813502d7d3993d641f9e1
zhangting005 <245892730@qq.com>
2020-05-24 11:57:13 +0800

    push

--- gugaoedu-catalina/gupaoedu-vip-strategy/src/com/gupaoedu/vip/strategy/Comparator.java
+++
@@ -1,8 +0,0 @@
-package com.gupaoedu.vip.strategy;
-
-//±È½ÏÆ÷
-public interface Comparator {
-	
-	int compareTo(Object obj1,Object obj2);
-	
-}
--- gupaoedu-vip-proxy/src/com/gupaoedu/vip/custom/GPInvocationHandler.java
+++
@@ -1,7 +0,0 @@
-package com.gupaoedu.vip.custom;
-
-import java.lang.reflect.Method;
-
-public interface GPInvocationHandler {
-	public Object invoke(Object proxy, Method method, Object[] args) throws Throwable;
-}
